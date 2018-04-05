e943bf46a11c910a68b3dc2becff7e05ffa5b167
Ben Mildren <mildrenben@gmail.com>
2018-04-05 15:14:06 +0100

    Added Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  border-radius: 3px;
+  &:hover {
+    background: rgba(0,0,0,0.07);
+  }
+`
+  
+export default Button
